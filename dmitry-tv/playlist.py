bb9de103b77cf4f343b979a19a2aaef9a996935b
Andrey <kms.amur@gmail.com>
2024-09-08 14:51:01 +1000

    update

--- dmitry-tv/playlist.py
+++ dmitry-tv/playlist.py
@@ -15,9 +15,14 @@ def get_url() -> list:
 
 def write_playlist() -> None:
     text: list = get_url()
+    ignore = 'http://player.smotrim.ru/iframe/stream/live_id'
+    subprocess.run(f'echo "{text.pop(0).strip()}" > modified_{FILE}', shell=True)
     with open(f'modified_{FILE}', 'a', encoding='utf-8') as playlist:
         for number, line in enumerate(text):
-            if line.startswith('http'):
+            if line.startswith(ignore):
+                playlist.write(f'{text[number - 1]}{line}')
+                print(line, end='')
+            elif line.startswith('http'):
                 cmd = f'./tv.sh {line.strip()}'
                 link = subprocess.getoutput(cmd)
                 if link.startswith('http'):
