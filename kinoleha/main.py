ee2e4181a6fd86e462b221c2f1f49aedc915f145
Andrey <kms.amur@gmail.com>
2026-03-06 15:42:13 +1000

    fix

--- kinoleha/main.py
+++ kinoleha/main.py
@@ -1,7 +1,7 @@
 from parser_site import main as site_parser
 
-# categories = ('vse-filmy', 'russkie', 'genre/priklyucheniya', '2023-god',
-#               'genre/komedii', 'top', 'genre/dokumentalqnyee')
+# categories = ('vse-filmy', 'russkie', 'genre/priklyuchenija', '2026-goda',
+#               'genre/komediya', 'top', 'genre/dokumentalqnyee')
 categories = ('vse-filmy',)
 
 
--- rutube/rutube_hls_movies.sh
+++ rutube/rutube_hls_movies.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -
 # $> ./rutube_hls_movies.sh
-# Ссылки rutube.ru
+# Прямые ссылки rutube.ru
 
 playlist='rutube_films_hls.m3u'
 echo "#EXTM3U" > "$playlist"
@@ -9,9 +9,9 @@ curl -s 'https://rutube.ru/feeds/movies/' | \
 grep -o 'https://rutube.ru/video/[a-z0-9]\{32\}/' | \
 sed 's/video/api\/play\/options/g' | \
 sort -u | while read url; do
-  link=$(curl -s "$url")
-  echo "$link" | yq -r '"#EXTINF:-1, " + .title' >> "$playlist"
-  links=$(echo "$link" | yq -r '.video_balancer.m3u8')
-  curl -s "$links" | tail -n 1 >> "$playlist"
+  api_url=$(curl -s "$url")
+  echo "$api_url" | yq -r '"#EXTINF:-1, " + .title' >> "$playlist"
+  direct_link=$(echo "$api_url" | yq -r '.video_balancer.m3u8')
+  curl -s "$direct_link" | tail -n 1 >> "$playlist"
 done
 
